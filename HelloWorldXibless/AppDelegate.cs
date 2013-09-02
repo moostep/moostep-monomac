d7fd66bb3626bb9d0ad4cd17f3aab074554a37cb
Daniil Rybin <daniil.rybin@gmail.com>
2013-09-02 21:46:54 +0200

    added application menu with 'quit' menu item

--- HelloWorldXibless/AppDelegate.cs
+++ HelloWorldXibless/AppDelegate.cs
@@ -9,16 +9,40 @@ namespace HelloWorldXibless
 	{
 		public override void FinishedLaunching (NSObject notification)
 		{
+			var appName = NSProcessInfo.ProcessInfo.ProcessName;
+
+			CreateMenu (appName);
+
 			var window = new NSWindow (
 				new RectangleF (0, 0, 400, 300),
 				NSWindowStyle.Titled,
 				NSBackingStore.Buffered,
 				false) {
-				Title =  "Hello World Xibless"
+				Title =  appName
 			};
 
 			window.CascadeTopLeftFromPoint (new PointF (20, 20));
 			window.MakeKeyAndOrderFront (null);
+		}
+
+		void CreateMenu (string appName)
+		{
+			var mainMenu = new NSMenu();
+
+			var appMenuItem = new NSMenuItem ();
+			mainMenu.AddItem (appMenuItem);
+
+			var appMenu = new NSMenu ();
+
+			var quitMenuItem = new NSMenuItem (String.Format ("Quit {0}", appName), "q", delegate {
+				NSApplication.SharedApplication.Terminate(mainMenu);
+			});
+
+			appMenu.AddItem (quitMenuItem);
+
+			appMenuItem.Submenu = appMenu;
+
+			NSApplication.SharedApplication.MainMenu = mainMenu;
 		}
 	}
 }
